b27403b67a0b7b4cd8e0920fcb6ed87ad7ad1b61
Ebenezer Tetteh <tebenezer25@gmail.com>
2023-03-16 16:29:50 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+ #include <stdio.h>
+/**
+ * main - Function that prints using printf
+ *
+ * Return: Returning 0
+ */
+int main(void)
+{printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
