403cea6958f6ded26427f16a5b67b11c61274d13
Ebenezer Tetteh <tebenezer25@gmail.com>
2023-03-16 18:18:35 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - C checks here first
+ *
+ * Return: return 1
+ */
+int main(void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+		return (1);
+}
