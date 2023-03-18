80e7d63d74cb682ca1800d678c4f9ce087bff038
Ebenezer Tetteh <tebenezer25@gmail.com>
2023-03-17 18:27:47 -0700

    single numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ *
+ * Return: always return 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
