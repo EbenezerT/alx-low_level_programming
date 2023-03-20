7dfef0b0b332175379e2d593494b93bbeb3bba0a
Ebenezer Tetteh <tebenezer25@gmail.com>
2023-03-20 15:21:04 -0700

    combination of numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - the main function goes here
+ *
+ * Return: Always return 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 1; j < 10; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
