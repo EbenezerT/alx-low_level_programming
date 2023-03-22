c3bcfc969fd276b1741668b2e5b33c532af21438
Ebenezer Tetteh <tebenezer25@gmail.com>
2023-03-22 14:33:31 -0700

    Absolute values

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *_abs - function computes absolute value of integer
+ *@c: parameter
+ *Return: always return an integer
+ */
+int _abs(int c)
+{
+	return (abs(c));
+}
