4863acfb821153456a74c53bb4a3168a13f56ff9
Ebenezer Tetteh <tebenezer25@gmail.com>
2023-03-23 15:48:31 -0700

    prints numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *print_numbers - prints numbers
+ * Return: Always return 0
+ */
+
+void print_numbers(void)
+{
+	int j;
+
+	for (j = 0; j < 10; j++)
+		_putchar(j + '0');
+	_putchar('\n');
+}
