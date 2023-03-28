1eb822f87c0635c06deda7f964995fb76e2cd4c9
Ebenezer Tetteh <tebenezer25@gmail.com>
2023-03-28 10:07:59 -0700

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _puts - prints string
+ * @str: parameter
+ * Return: always return 0
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,7 +7,8 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int main(void);
 int _strlen(char *s);
-
+void _puts(char *str);
+int _putchar(char c);
 
 
 #endif
