1bca714491ccffe4c050884027ab8110eee222ce
Ebenezer Tetteh <tebenezer25@gmail.com>
2023-03-28 15:49:54 -0700

    reverse printing

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - print string in reverse form
+ * @s: parameter
+ *Return: always return 0
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		count++;
+	for (i = count; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,6 +9,8 @@ int main(void);
 int _strlen(char *s);
 void _puts(char *str);
 int _putchar(char c);
+void print_rev(char *s);
+
 
 
 #endif
