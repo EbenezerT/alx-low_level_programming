d99c4e5f94733af0707dd4a4c1db5c6b221a3109
Ebenezer Tetteh <tebenezer25@gmail.com>
2023-03-28 05:08:08 -0700

    ressetting pointer value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function to reset pointer value
+ * @n: Parameter to be input
+ */
+
+void reset_to_98(int *n)
+{
+	int updated;
+
+	updated = 98;
+	*n = updated;
+}
