f81806c70e7f82ea8d5894da6f346381ab7559b4
Ebenezer Tetteh <tebenezer25@gmail.com>
2023-03-28 17:11:59 -0700

    every character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <string.h>
 
-/*
+/**
  * puts2 - prints every other character
  * @str: parameter to input
  */
@@ -9,11 +9,11 @@
 void puts2(char *str)
 {
 	int i;
-        int count = 0;
+	int count = 0;
 
-        for (i = 0; str[i] != '\0'; i++)
-        {
-                count++;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		count++;
 	}
 	for (i = 0; i < count; i += 2)
 		_putchar(str[i]);
