91ec69700c193a7fc90f256d134560de771f6e0b
Ebenezer Tetteh <tebenezer25@gmail.com>
2023-03-29 09:33:19 -0700

    comparing two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strcmp - comparing two strings
+ * @s1: parameter 1
+ * @s2: parameter 2
+ * Return: Alsways return 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	result = strcmp(s1, s2);
+	return (result);
+}
