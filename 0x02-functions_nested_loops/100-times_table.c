eae55464f7177db5189764ccd2b461ffd4bfda1c
Ebenezer Tetteh <tebenezer25@gmail.com>
2023-04-02 08:34:53 -0700

    n times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * print_times_table - print n times table
+ * @n: number times
+ */
+
+void print_times_table(int n)
+{
+	int row, col, prod;
+
+	if (n > 0 && n < 15)
+	{
+		for (row = 0; row <= n; row++)
+		{
+			printf("0");
+			printf(" ");
+			for (col = 1; col <= n; col++)
+			{
+				prod = row * col;
+				if (prod <= 99)
+					printf(" ");
+				if (prod <= 9)
+					printf(" ");
+				if (prod >= 100)
+				{
+					printf("%d", prod / 100);
+					printf("%d", (prod / 10) % 10);
+				}
+				else if (prod >= 10 && prod <= 99)
+				{
+					printf("%d", prod / 10);
+				}
+				printf("%d", prod % 10);
+			}
+			printf("\n");
+		}
+	}
+}
