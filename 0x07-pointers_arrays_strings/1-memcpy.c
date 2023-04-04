0f556e72ffbd1ccfb6fc3142b1104dc1422ca58c
Ebenezer Tetteh <tebenezer25@gmail.com>
2023-04-04 08:25:48 -0700

    copies memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,7 +11,9 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int index;
 
-	for (index = 0; index <= n; index++)
+	for (index = 0; index < n; index++)
 		dest[index] = src[index];
 	return (dest);
+
+	return (memcpy(dest, src, n));
 }
