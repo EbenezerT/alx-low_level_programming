9d03e99213541fb174ab9f2de26098bfd7aef195
Ebenezer Tetteh <tebenezer25@gmail.com>
2023-04-10 03:00:39 -0700

    prints result of adding numbers

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -9,6 +9,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[]);
 int _putchar(char c);
 int main(int argc, char __attribute__((__unused__)) *argv[]);
 int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
 
 
 
