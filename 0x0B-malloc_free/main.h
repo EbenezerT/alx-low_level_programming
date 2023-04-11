2da11194c418367ec63114a82fcfdf5464464c6c
Ebenezer Tetteh <tebenezer25@gmail.com>
2023-04-11 06:22:46 -0700

    function that creates an array of chars, and initializes it with a specific char

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdlib.h>
+
+void simple_print_buffer(char *buffer, unsigned int size);
+char *create_array(unsigned int size, char c);
+int main(void);
+
+
+
+
+#endif
