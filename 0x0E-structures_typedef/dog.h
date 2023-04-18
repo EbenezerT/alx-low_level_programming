cb8e031a651e8e12b86fcf995cecffca432cd071
Ebenezer Tetteh <tebenezer25@gmail.com>
2023-04-18 16:25:41 -0700

    a new type struct dog with elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef _STRUCT_H
+#define _STRUCT_H
+
+/**
+ * struct dog - dog details
+ * @name: the name of the dog
+ * @age: the age
+ * @owner: owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+
+
+#endif
