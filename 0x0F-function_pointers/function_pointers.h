3dafa4330881ebb94dd6d177d5dd9897d87fe045
Ebenezer Tetteh <tebenezer25@gmail.com>
2023-04-19 05:36:20 -0700

    action on an array

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,10 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
+#include <stddef.h>
 
 void print_name(char *name, void (*f)(char *));
 int main(void);
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 
 
